407658f3770f3aa05a0decbef86332a39c6af2b0
Bonnerue <thomas.bonnerue@akretion.com>
2025-06-03 17:18:06 +0200

    [MIG] donation_base: Migration to 18.0

--- donation_base/models/product.py
+++ donation_base/models/product.py
@@ -10,18 +10,28 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    detailed_type = fields.Selection(
+    service_tracking = fields.Selection(
         selection_add=[
             ("donation", "Donation"),
-            ("donation_in_kind_consu", "In-Kind Donation Consummable"),
             ("donation_in_kind_service", "In-Kind Donation Service"),
         ],
         ondelete={
-            "donation": "set service",
-            "donation_in_kind_consu": "set consu",
-            "donation_in_kind_service": "set service",
+            "donation": "set default",
+            "donation_in_kind_service": "set default",
         },
     )
+    # service_tracking = fields.Selection(
+    #     selection_add=[
+    #         ("donation", "Donation"),
+    #         ("donation_in_kind_consu", "In-Kind Donation Consummable"),
+    #         ("donation_in_kind_service", "In-Kind Donation Service"),
+    #     ],
+    #     ondelete={
+    #         "donation": "set service",
+    #         "donation_in_kind_consu": "set consu",
+    #         "donation_in_kind_service": "set service",
+    #     },
+    # )
     tax_receipt_ok = fields.Boolean(
         string="Is Eligible for a Tax Receipt",
         tracking=True,
@@ -32,34 +42,23 @@ class ProductTemplate(models.Model):
         help="Specify if the product is eligible for a tax receipt",
     )
 
-    @api.depends("detailed_type")
+    @api.depends("service_tracking")
     def _compute_tax_receipt_ok(self):
         for product in self:
-            if product.detailed_type and not product.detailed_type.startswith(
+            if product.service_tracking and not product.service_tracking.startswith(
                 "donation"
             ):
                 product.tax_receipt_ok = False
 
-    def _detailed_type_mapping(self):
-        res = super()._detailed_type_mapping()
-        res.update(
-            {
-                "donation": "service",
-                "donation_in_kind_consu": "consu",
-                "donation_in_kind_service": "service",
-            }
-        )
-        return res
-
-    @api.onchange("detailed_type")
+    @api.onchange("service_tracking")
     def _donation_change(self):
         for product in self:
-            if product.detailed_type == "donation":
+            if product.service_tracking == "donation":
                 product.taxes_id = False
                 product.supplier_taxes_id = False
                 product.purchase_ok = False
 
-    @api.constrains("detailed_type", "taxes_id")
+    @api.constrains("service_tracking", "taxes_id")
     def donation_check(self):
         for product in self:
             # The check below is to make sure that we don't forget to remove
@@ -67,7 +66,7 @@ class ProductTemplate(models.Model):
             # for users of donation_sale. If there are countries that have
             # sale tax on donations (!), please tell us and we can remove this
             # constraint
-            if product.detailed_type == "donation" and product.taxes_id:
+            if product.service_tracking == "donation" and product.taxes_id:
                 raise ValidationError(
                     _(
                         "There shouldn't have any Customer Taxes on the "
@@ -76,14 +75,17 @@ class ProductTemplate(models.Model):
                     % product.display_name
                 )
 
+    def _service_tracking_blacklist(self):
+        return super()._service_tracking_blacklist() + ["donation"]
+
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    @api.onchange("detailed_type")
+    @api.onchange("service_tracking")
     def _donation_change(self):
         for product in self:
-            if product.detailed_type == "donation":
+            if product.service_tracking == "donation":
                 product.taxes_id = False
                 product.supplier_taxes_id = False
                 product.purchase_ok = False
