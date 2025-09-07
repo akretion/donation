a31da465f057ee1412212ce698a74c7ab399404d
Florian du Garage Num <makayabou@gmail.com>
2025-09-07 18:13:45 +0200

    is_donation boolean

--- donation_base/models/product.py
+++ donation_base/models/product.py
@@ -10,14 +10,21 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    is_donation = fields.Selection(
-        selection=[
-            ("donation", "Donation"),
-            ("donation_in_kind_service", "In-Kind Donation Service"),
-        ],
+    is_donation = fields.Boolean(
         string="Is a donation",
+        tracking=True,
         readonly=False,
+        store=True,
         help="Specify if the product is a donation",
+    )
+    in_kind = fields.Boolean(
+        string="Is an in-kind donation",
+        tracking=True,
+        compute="_compute_is_inkind",
+        readonly=False,
+        store=True,
+        precompute=True,
+        help="Specify if the donation item is of type in-kind (a good or a service rather than a monetary donation)"
     )
     tax_receipt_ok = fields.Boolean(
         string="Is Eligible for a Tax Receipt",
@@ -26,8 +33,16 @@ class ProductTemplate(models.Model):
         readonly=False,
         store=True,
         precompute=True,
-        help="Specify if the product is eligible for a tax receipt",
+        help="Specify if the donation item is eligible for a tax receipt",
     )
+
+    @api.depends("is_donation")
+    def _compute_is_inkind(self):
+        for product in self:
+            if not product.is_donation:
+                product.in_kind = False
+            elif product.type in ["consu", "combo"]:
+                product.in_kind = True
 
     @api.depends("is_donation")
     def _compute_tax_receipt_ok(self):
@@ -38,11 +53,25 @@ class ProductTemplate(models.Model):
     @api.onchange("is_donation")
     def _donation_change(self):
         for product in self:
-            if product.donation:
+            if product.is_donation:
                 product.taxes_id = False
                 product.supplier_taxes_id = False
                 product.purchase_ok = False
-
+                product.sale_ok = False
+                if "can_be_expensed" in product._fields:
+                    product.can_be_expensed = False
+                    
+    @api.constrains("type", "in_kind")
+    def inkind_check(self):
+        for product in self:
+            if product.type in ["consu", "combo"] and product.in_kind == False:
+                raise ValidationError(
+                    _(
+                        "A donation product of type '%s' "
+                        "is always an in-kind donation"
+                    )
+                    % product.type
+                )
     @api.constrains("is_donation", "taxes_id")
     def donation_check(self):
         for product in self:
@@ -71,3 +100,6 @@ class ProductProduct(models.Model):
                 product.taxes_id = False
                 product.supplier_taxes_id = False
                 product.purchase_ok = False
+                product.sale_ok = False
+                if "can_be_expensed" in product._fields:
+                    product.can_be_expensed = False
